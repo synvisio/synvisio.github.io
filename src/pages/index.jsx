948ef5c148eca43d2f826d9d92ddfd86af499a32
kiranbandi <bvenkatkiran@gmail.com>
2018-08-30 12:45:35 -0600

    reorder content

--- src/pages/index.jsx
+++
@@ -1,4 +0,0 @@
-export { default as NotFound } from './NotFound';
-export { default as Home } from './Home';
-export { default as Dashboard } from './Dashboard';
-export { default as Configuration } from './Configuration';
